ec882c619e38beac1232a1e59a7273f6b9b36304
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-09-21 20:02:51 -0600

    Added Models, controllers and views.

---
+++ BooksTracker/BooksTracker/Controllers/BookController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BooksTracker.Controllers;
+using BooksTracker.Models;
+using Microsoft.VisualBasic;
+
+namespace BooksTracker.Controllers
+{
+    public class BookController : Controller
+    {
+        //A public static "Books" property which is a list of "Book" objects.
+        //This will be replaced by a proper database on {Day 2 assignment title}.
+        static public List<Book> Books { get; set; } = new List<Book>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /**
+         * Method "CreateBook()".
+            Accepts the same parameters as the "Book" constructor.
+            Creates and adds a "Book" to the "Books" list.
+            Ensures the provided ID is unique in the list.
+            Throw an exception if the ID already exists.
+         */
+        public IActionResult CreateBook(string id, string title, string author, DateTime publicationDate, DateTime checkOutDate, DateTime dueDate)
+        {
+            // A request has come in that has some data stored in the query (GET or POST).
+            if (Request.Query.Count > 0)
+            {
+                if (id != null && title != null && author != null && publicationDate != null && checkOutDate != null && dueDate != null)
+                {
+                    // All expected data provided, so this will be our submit state.
+                    Books.Add(new Book()
+                    {
+                      //ID = int.Parse(id),
+                       // Title = title,
+                       // Author = author,
+                      //  PublicationDate = publicationDate,
+                        CheckedOutDate = checkOutDate,
+                        DueDate = dueDate,
+                        ReturnedDate = null,
+                    });
+                    ViewBag.Success = $"You have successfully checked out {title} until {dueDate} ";
+                }
+                else
+                {
+                    // All expected data not provided, so this will be our error state.
+                    ViewBag.Error = "Unable to check out book";
+                    // Store our data to re-add to the form.
+                    ViewBag.ID = id;
+                    ViewBag.Title = title;
+                    ViewBag.Author = author;
+                    ViewBag.PublicationDate = publicationDate;
+                    ViewBag.CheckOutDate = checkOutDate;
+                    ViewBag.DueDate = checkOutDate.AddDays(14);
+                    ViewBag.ReturnedDate = null;
+                }
+            }
+            // else
+            // No request, so this will be our inital state.
+            return View();
+        }
+        public IActionResult List()
+        {
+            ViewBag.Book = Books;
+            return View();
+        }
+        public IActionResult Details(string id, string delete)
+        {
+            IActionResult result;
+            if (delete != null)
+            {
+                DeleteBookByID(int.Parse(id));
+                result = RedirectToAction("List");
+            }
+            else
+            {
+                ViewBag.Book = GetBookByID(int.Parse(id));
+                result = View();
+            }
+            return result;
+        }
+        // Method "ExtendDueDateForBookByID()".
+        // Extensions are 7 days from the current date(7 days from when the user requests the extension, not 7 days past the "DueDate").
+        public DateTime ExtendDueDateForBookById(int id)
+        {
+             DateTime DueDate = DateTime.Now.AddDays(7);
+            return DueDate;
+        }
+        //Method "GetBookByID()".
+        // Returns the book with the given ID from the "Books" list.
+        public Book GetBookByID(int id)
+        {
+            return Books.Where(x => x.ID == id).Single();
+        }
+        //Method "DeleteBookByID()".
+        //Removes the book with the given ID from the "Books" list.
+        public void DeleteBookByID(int id)
+        {
+            Books.Remove(Books.Where(x => x.ID == id).Single());
+        }
+
+        /*
+         * ActionResult “ViewBooks”
+        Display: "You checked out {title} on {CheckedOutDate}, and it {is/was} due on {DueDate}."
+        Use conditional rendering to make a choice about using ‘is’ or ‘was’ based on today’s date.
+
+         */
+       /* public Book ViewBookByID(int id)
+        {
+            if (Books.CheckedOutDate.date == DateTime.Now)
+            {
+               Books.Where(Books.Where(x => x.CheckedOutDate == DateTime.Now));
+            }
+        }*/
+    }
+}
--- BooksTracker/BooksTracker/Models/Book.cs
+++ BooksTracker/BooksTracker/Models/Book.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,10 +8,111 @@ namespace BooksTracker.Models
 {
     public class Book
     {
-        public string Title { get;}
+        /*
+         * Book class (Model):
+            string “Title”
+            This property should be readOnly (getter only, backing variable initialized via constructor)
 
-        public DateTime PublicationDate { get; }
+            DateTime “PublicationDate”
+            This property should be readOnly (getter only, backing variable initialized via constructor)
+
+            DateTime “CheckedOutDate”
+
+            DateTime “DueDate”
+            This will equal “CheckedOutDate” + 14 days (set in constructor)
+            This will update with each extension (via the ExtendDueDateForBookByID() method)
+
+            DateTime “ReturnedDate”
+            Default value should be null (set in constructor)
+
+            string “Author”
+            This property should be readOnly (getter only, backing variable initialized via constructor)
+
+            int “ID”
+            This property should be readOnly (getter only, backing variable initialized via constructor)
+            This property will be auto-incremented by the database in tomorrow’s practice
+            User will have to add “ID” on Day 1 and the code will have to validate that the “ID” is unique (in the CreateBook() method)
+
+            Constructor accepting the ID, Title, Author, PublicationDate and CheckedOutDate as parameters
+
+            “DueDate” will be set to 14 days after “CheckedOutDate”
+
+            “ReturnedDate” will be set to null
+
+         */
+
+        private string _title;
+        public string Title { get
+            {
+                return _title;
+            }
+                }
+
+        private DateTime _publicationdate;
+        public DateTime PublicationDate
+        {
+            get
+            {
+                return _publicationdate;
+            }
+        }
 
         public DateTime CheckedOutDate { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public DateTime? ReturnedDate { get; set; }
+
+        private string _author;
+        public string Author { 
+                get
+            {
+                return _author;
+            }
+        }
+
+        private int _id;
+        public int ID { get
+            {
+                return _id;
+            }
+        }
+
+
+        public Book()
+        {
+            this._id = 1;
+            this._title = "Nineteen Eighty-Four";
+            this._author = "George Orwell"; 
+            this._publicationdate = new DateTime(1996 / 08 / 01);
+            CheckedOutDate = new DateTime(2020 / 09 / 21);
+            DueDate = CheckedOutDate.AddDays(14);
+            ReturnedDate = null;
+
+        }
+
+        public Book(int id,string title, string author, DateTime publicationDate, DateTime checkoutdate)
+        {
+            this._id= id;
+            this._title = title;
+            this._author = author;
+            this._publicationdate = publicationDate;
+            DueDate = checkoutdate.AddDays(14);
+            ReturnedDate = null;
+
+        }
+
+        public static void ExtendDueDateForBookByID()
+        {
+           
+
+        }
+
+        public static void CreateBook()
+        {
+
+        }
+
     }
+
+    
 }
