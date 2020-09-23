b2cb52990be42c7f8da2c151a7c2655df0f71d8a
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-09-23 16:11:59 -0600

    Updated code with in class practice solution.

--- BooksTracker/BooksTracker/Controllers/BookController.cs
+++ BooksTracker/BooksTracker/Controllers/BookController.cs
@@ -11,136 +11,149 @@ namespace BooksTracker.Controllers
 {
     public class BookController : Controller
     {
-        //A public static "Books" property which is a list of "Book" objects.
-        //This will be replaced by a proper database on {Day 2 assignment title}.
+        /*
+         BookController : Controller class:
+            Action/View “Create”
+                Will display the form to create an object. 
+                If the appropriate parameters are supplied in the query, attempt creation of the “Book” and add it to the list.
+                If the ID is already in the list, throw an exception.
+                Success message: "You have successfully checked out {title} until {DueDate}."
+                Error Message: “Unable to check out book: {Exception.Message}.”
+            Action/View “List”
+                Render a list of all books as links that will load the “Details” Action/View.
+            Action/View “Details”
+                If no get parameter “id” was supplied, render “No book selected.”
+                If an “id” get parameter was supplied, use GetBookByID() and render:
+                "You checked out {title} on {CheckedOutDate}, and it {is/was} due on {DueDate}."
+                Use conditional rendering to make a choice about using ‘is’ or ‘was’ based on today’s date.
+                A button that will call ExtendDueDateForBookByID().
+                A button that will call DeleteBookByID().
+            Method “CreateBook()”.
+                Accepts the same parameters as the “Book” constructor.
+                Creates and adds a “Book” to the “Books” list.
+                Ensures the provided ID is unique in the list.
+                Throw an exception if the ID already exists.
+            Method “GetBookByID()”.
+                Returns the book with the given ID from the “Books” list.
+            Method “ExtendDueDateForBookByID()”.
+                Extensions are 7 days from the current date (7 days from when the user requests the extension, not 7 days past the “DueDate”).
+            Method “DeleteBookByID()”.
+                Removes the book with the given ID from the “Books” list.
+            A public static “Books” property which is a list of “Book” objects.
+                This will be replaced by a proper database on {Day 2 assignment title}.
+            */
         static public List<Book> Books { get; set; } = new List<Book>();
-      /*  public IActionResult Create()
+
+        public IActionResult Index()
         {
-            return View();
-        }*/
-        /**
-         * Method "CreateBook()".
-            Accepts the same parameters as the "Book" constructor.
-            Creates and adds a "Book" to the "Books" list.
-            Ensures the provided ID is unique in the list.
-            Throw an exception if the ID already exists.
-         */
-        public IActionResult Create(string id, string title, string author, DateTime publicationDate, DateTime checkOutDate, DateTime dueDate)
+            return RedirectToAction("List");
+        }
+        public IActionResult Create(string id, string title, string author, string publicationDate, string checkedOutDate)
         {
-            // A request has come in that has some data stored in the query (GET or POST).
             if (Request.Query.Count > 0)
             {
-                if (id != null && title != null && author != null && publicationDate != null && checkOutDate != null && dueDate != null)
+                try
                 {
-                    // All expected data provided, so this will be our submit state.
-                    Books.Add(new Book()
+                    if (id != null && title != null && author != null && publicationDate != null && checkedOutDate != null)
                     {
-                      //ID = int.Parse(id),
-                       // Title = title,
-                       // Author = author,
-                      //  PublicationDate = publicationDate,
-                        CheckedOutDate = checkOutDate,
-                        DueDate = dueDate,
-                        ReturnedDate = null,
-                    });
+                        // Get parameters come in as a string, so we have to convert those to the data types required.
+                        Book createdBook = CreateBook(int.Parse(id), title, author, DateTime.Parse(publicationDate), DateTime.Parse(checkedOutDate));
 
-                    ViewBag.Success = $"You have successfully checked out {title} until {dueDate} ";
+                        ViewBag.Success = $"You have successfully checked out {createdBook.Title} until {createdBook.DueDate}.";
+                    }
+                    else
+                    {
+                        throw new Exception("Not all fields provided for book creation.");
+                    }
                 }
-                else
+                catch (Exception e)
                 {
                     // All expected data not provided, so this will be our error state.
-                    ViewBag.Error = "Unable to check out book";
+                    ViewBag.Error = $"Unable to check out book: {e.Message}";
+
                     // Store our data to re-add to the form.
                     ViewBag.ID = id;
-                    ViewBag.Title = title;
+                    ViewBag.BookTitle = title;
                     ViewBag.Author = author;
                     ViewBag.PublicationDate = publicationDate;
-                    ViewBag.CheckOutDate = checkOutDate;
-                    ViewBag.DueDate = checkOutDate.AddDays(14);
-                    ViewBag.ReturnedDate = null;
+                    ViewBag.CheckedOutDate = checkedOutDate;
                 }
             }
             // else
             // No request, so this will be our inital state.
+
             return View();
         }
         public IActionResult List()
         {
-            ViewBag.Book = Books;
+            ViewBag.Books = Books;
+
             return View();
         }
-        public IActionResult Details(string id, string delete)
+        public IActionResult Details(string id, string delete, string extend)
         {
             IActionResult result;
-            if (delete != null)
+
+            // If ID wasn't provided, or if it won't parse to an int, or the ID doesn't exist.
+            if (id == null || !int.TryParse(id, out int temp) || Books.Where(x => x.ID == int.Parse(id)).Count() < 1)
             {
-                DeleteBookByID(int.Parse(id));
-                result = RedirectToAction("List");
+                ViewBag.Error = "No book selected.";
+                result = View();
             }
             else
             {
-                ViewBag.Book = GetBookByID(int.Parse(id));
-                result = View();
+                if (delete != null)
+                {
+                    DeleteBookByID(int.Parse(id));
+                    result = RedirectToAction("List");
+                }
+                else
+                {
+                    if (extend != null)
+                    {
+                        ExtendDueDateForBookByID(int.Parse(id));
+                    }
+                    ViewBag.Book = GetBookByID(int.Parse(id));
+                    result = View();
+                }
             }
             return result;
         }
-
-        // Method "ExtendDueDateForBookByID()".
-
-        // Extensions are 7 days from the current date(7 days from when the user requests the extension, not 7 days past the "DueDate").
-        public DateTime ExtendDueDateForBookById(int id)
+        public Book CreateBook(int id, string title, string author, DateTime publicationDate, DateTime checkedOutDate)
         {
-             DateTime DueDate = DateTime.Now.AddDays(7);
-            return DueDate;
-        }
+            /*
+            Accepts the same parameters as the “Book” constructor.
+            Creates and adds a “Book” to the “Books” list.
+            Ensures the provided ID is unique in the list.
+            Throw an exception if the ID already exists.
+            */
+            if (Books.Where(x => x.ID == id).Count() > 0)
+            {
+                throw new Exception("That ID already exists.");
+            }
 
-        //Method "GetBookByID()".
-        // Returns the book with the given ID from the "Books" list.
+            Book newBook = new Book(id, title, author, publicationDate, checkedOutDate);
+            Books.Add(newBook);
+            return newBook;
+        }
         public Book GetBookByID(int id)
         {
+            // Returns the book with the given ID from the “Books” list.
             return Books.Where(x => x.ID == id).Single();
         }
-
-        //Method "DeleteBookByID()".
-        //Removes the book with the given ID from the "Books" list.
-
+        public void ExtendDueDateForBookByID(int id)
+        {
+            // Extensions are 7 days from the current date (7 days from when the user requests the extension, not 7 days past the “DueDate”).
+            GetBookByID(id).DueDate = DateTime.Now.Date.AddDays(7);
+        }
         public void DeleteBookByID(int id)
         {
+            // Removes the book with the given ID from the “Books” list.
             Books.Remove(Books.Where(x => x.ID == id).Single());
         }
-
-        /*
-         * ActionResult “ViewBooks”
-        Display: "You checked out {title} on {CheckedOutDate}, and it {is/was} due on {DueDate}."
-        Use conditional rendering to make a choice about using ‘is’ or ‘was’ based on today’s date.
-
-         */
-
-       public IActionResult ViewBookByID(string id, string title, DateTime checkOutDate)
-        {
-           
-            ViewBag.ID = id;
-            ViewBag.Title = title;           
-            ViewBag.CheckOutDate = checkOutDate;
-            ViewBag.DueDate = checkOutDate.AddDays(14);
-
-
-            if (ViewBag.CheckOutDate == DateTime.Now)
-            { 
-           
-
-                ViewBag.Book = GetBookByID(int.Parse(id));
-                ViewBag.Success = $"You checked out {title} on {ViewBag.CheckOutDate}, and it is due on {ViewBag.DueDate} ";
-            }
-
-            else
-            {
-                ViewBag.Success = $"You checked out {title} on {ViewBag.CheckOutDate}, and it was due on {ViewBag.DueDate.Subtract(DateTime.Now)} ";
-            }
-
-            return View();
-        }
     }
-
-    // Code borowed: @ link https://github.com/TECHCareers-by-Manpower/4.1-MVC/tree/master/MVC_4Point1
 }
+
+// Code borowed: @ link https://github.com/TECHCareers-by-Manpower/4.1-MVC/tree/master/MVC_4Point1
+
+//Code borowed: @ link https://github.com/TECHCareers-by-Manpower/4.1-MVC/blob/Sep21Practice/MVC_4Point1/Controllers/BookController.cs
--- BooksTracker/BooksTracker/Models/Book.cs
+++ BooksTracker/BooksTracker/Models/Book.cs
@@ -6,108 +6,68 @@ using System.Threading.Tasks;
 
 namespace BooksTracker.Models
 {
+    /*
+       Book class (Model):
+          string “Title”
+              This property should be readOnly (getter only, backing variable initialized via constructor)
+          DateTime “PublicationDate”
+              This property should be readOnly (getter only, backing variable initialized via constructor)
+          DateTime “CheckedOutDate”
+          DateTime “DueDate”
+              This will equal “CheckedOutDate” + 14 days (set in constructor)
+              This will update with each extension (via the ExtendDueDateForBookByID() method)
+          DateTime “ReturnedDate”
+              Default value should be null (set in constructor)
+          string “Author”
+              This property should be readOnly (getter only, backing variable initialized via constructor)
+          int “ID”
+              This property should be readOnly (getter only, backing variable initialized via constructor)
+              This property will be auto-incremented by the database in tomorrow’s practice
+              User will have to add “ID” on Day 1 and the code will have to validate that the “ID” is unique (in the CreateBook() method)
+          Constructor accepting the ID, Title, Author, PublicationDate and CheckedOutDate as parameters
+              “DueDate” will be set to 14 days after “CheckedOutDate”
+              “ReturnedDate” will be set to null
+          */
     public class Book
     {
-        /*
-         * Book class (Model):
-            string “Title”
-            This property should be readOnly (getter only, backing variable initialized via constructor)
-
-            DateTime “PublicationDate”
-            This property should be readOnly (getter only, backing variable initialized via constructor)
-
-            DateTime “CheckedOutDate”
-
-            DateTime “DueDate”
-            This will equal “CheckedOutDate” + 14 days (set in constructor)
-            This will update with each extension (via the ExtendDueDateForBookByID() method)
-
-            DateTime “ReturnedDate”
-            Default value should be null (set in constructor)
-
-            string “Author”
-            This property should be readOnly (getter only, backing variable initialized via constructor)
-
-            int “ID”
-            This property should be readOnly (getter only, backing variable initialized via constructor)
-            This property will be auto-incremented by the database in tomorrow’s practice
-            User will have to add “ID” on Day 1 and the code will have to validate that the “ID” is unique (in the CreateBook() method)
-
-            Constructor accepting the ID, Title, Author, PublicationDate and CheckedOutDate as parameters
-
-            “DueDate” will be set to 14 days after “CheckedOutDate”
-
-            “ReturnedDate” will be set to null
-
-         */
+        private int _id;
+        public int ID => _id;
 
         private string _title;
-        public string Title
-        {
-            get
-            {
-                return _title;
-            }
-        }
-
-        private DateTime _publicationdate;
-        public DateTime PublicationDate
-        {
-            get
-            {
-                return _publicationdate;
-            }
-        }
-
-        public DateTime CheckedOutDate { get; set; }
-        public DateTime DueDate { get; set; }
-
-        public DateTime? ReturnedDate { get; set; }
+        public string Title => _title;
 
         private string _author;
-        public string Author
+        public string Author => _author;
+        /*
         {
             get
             {
                 return _author;
             }
         }
+        */
 
-        private int _id;
-        public int ID
+        private DateTime _publicationDate;
+        public DateTime PublicationDate => _publicationDate;
+
+        public DateTime CheckedOutDate { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public DateTime? ReturnedDate { get; set; }
+
+
+        public Book(int id, string title, string author, DateTime publicationDate, DateTime checkedOutDate)
         {
-            get
-            {
-                return _id;
-            }
-        }
-
-
-        public Book()
-        {
-            this._id = 1;
-            this._title = "Nineteen Eighty-Four";
-            this._author = "George Orwell";
-            this._publicationdate = new DateTime(1949/06/08);
-            CheckedOutDate = new DateTime(2020/09/20);
+            _id = id;
+            _title = title;
+            _author = author;
+            _publicationDate = publicationDate;
+            CheckedOutDate = checkedOutDate;
             DueDate = CheckedOutDate.AddDays(14);
             ReturnedDate = null;
-
         }
-
-        public Book(int id, string title, string author, DateTime publicationDate, DateTime checkoutdate)
-        {
-            this._id = id;
-            this._title = title;
-            this._author = author;
-            this._publicationdate = publicationDate;
-            DueDate = checkoutdate.AddDays(14);
-            ReturnedDate = null;
-
-        }
-
     }
 }
 
-
 // Code borowed: @ link https://github.com/TECHCareers-by-Manpower/4.1-MVC/tree/master/MVC_4Point1
+// Code borowed: @ link https://github.com/TECHCareers-by-Manpower/4.1-MVC/blob/Sep21Practice/MVC_4Point1/Models/Book.cs
